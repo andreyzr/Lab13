0cefc88c442025b3a10e1fd002b719bf44452a6b
andreyzr43 <andreyzr43@yandex.ru>
2023-05-16 18:00:32 +0300

    доп

---
+++ PW/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Man man = new Man("Иванов", 2000, "Москва");
+            Console.WriteLine(man.Print());
+            Student student = new Student();
+            student.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PW/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW
+{
+    internal class Student:Man
+    {
+
+    }
+}
